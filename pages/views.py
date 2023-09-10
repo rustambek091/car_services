f5f84cdf95c017304cdd255bb6bdc04a22dc094b
rustambek091 <ismatovr898@gmail.com>
2023-09-10 21:52:27 +0500

    our_expert

--- pages/views.py
+++ pages/views.py
@@ -5,19 +5,26 @@ from .models import *
 def Home(request):
     work = Work_time.objects.all()
     service = Our_services.objects.all()
+    expert = Our_expert.objects.all()
     context = {
         'work': work,
         'service': service,
+        'expert' : expert, 
     }
     return render(request, 'index.html', context)
 
 def About(request):
     return render(request, 'about.html')
 
+
 def Booking(request):
     return render(request, 'booking.html')
     
 def Contact(request):
+    contact = Contact.objects.all()
+    context  = {
+        'contact':contact,
+    }
     return render(request, 'contact.html')
 
 def Service(request):
